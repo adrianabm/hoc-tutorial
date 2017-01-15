9489bf566e14f23babc0df77f69f66b4db767a10
Adriana Matsufuji <adrianabm@gmail.com>
2017-01-15 13:41:21 +0100

    Header component

---
+++ src/components/header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router'
+
+
+class Header extends Component {
+  authButton() {
+    return <button>Sign In</button>
+  }
+
+  render() {
+    return (
+      <nav className="navbar navbar-default">
+        <ul className="nav navbar-nav">
+          <li className="navbar-item">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/resources">Resources</Link>
+          </li>
+          <li className="navbar-item">
+            { this.authButton() }
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Header
